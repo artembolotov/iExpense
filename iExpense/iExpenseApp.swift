a2554b906f3beda7fb0d5fa21532b527e4e4688b
Artem Bolotov <teeemon@gmail.com>
2023-01-09 12:01:45 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by artembolotov on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
